290815e423c7a71afe3f2047a97d3f4bf7045f49
Fabian Kägy <mail@fabian-kaegy.de>
2024-09-24 07:50:57 +0200

    implement changes

--- themes/10up-theme/includes/blocks.php
+++ themes/10up-theme/includes/blocks.php
@@ -163,14 +163,20 @@ function enqueue_block_specific_styles() {
 			];
 		}
 
+		[$block_namespace, $block_name] = explode( '/', $block_type );
+
+		wp_register_style(
+				"tenup-theme-{$block_namespace}-{$block_name}",
+				TENUP_THEME_DIST_URL . 'blocks/autoenqueue/' . $block_type . '.css',
+				$asset_file['version'],
+				$asset_file['dependencies'],
+		);
+
 		wp_enqueue_block_style(
 			$block_type,
 			[
-				'handle'       => "{$block_type}",
-				'src'          => TENUP_THEME_DIST_URL . 'blocks/autoenqueue/' . $block_type . '.css',
+				'handle'       => "tenup-theme-{$block_namespace}-{$block_name}",
 				'path'         => $stylesheet_path,
-				'version'      => $asset_file['version'],
-				'dependencies' => $asset_file['dependencies'],
 			]
 		);
 	}
